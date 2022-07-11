20d18d3db550c75abbd32857bb41728364b32a88
ShellManager <374779789@qq.com>
2022-07-11 14:55:06 +0800

    update

--- docker/sub-update.sh
+++ docker/sub-update.sh
@@ -5,7 +5,7 @@ rootPath="/Sub-Store"
 backend="$rootPath/backend"
 web="$rootPath/Front"
 
-echo -e "======================== 1. 启动nginx ========================\n"
+echo -e "======================== 1. 重启nginx ========================\n"
 echo -e "生成 nginx 配置文件\n"
 envsubst '${ALLOW_IP}' < /etc/nginx/conf.d/front.template > /etc/nginx/conf.d/front.conf
 nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
