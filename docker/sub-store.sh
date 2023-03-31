effbc4a5d5c70aadc56404a5b318b401740db5db
dompling <374779789@qq.com>
2023-03-31 15:05:30 +0800

    更新优化

--- docker/sub-store.sh
+++ docker/sub-store.sh
@@ -30,7 +30,7 @@ echo -e "==============================================================\n"
 echo -e "======================== 3、启动后端接口 ========================\n"
 
     cp -r "$gitPath/Sub-Store/backend" "$rootPath"
-    cd "$backend" && pnpm install
+    cd "$backend" && pnpm install && pnpm build
     pm2 start sub-store.min.js --name "sub-store" --source-map-support --time
 
 echo -e "==============================================================\n"
