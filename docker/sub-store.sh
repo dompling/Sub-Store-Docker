7e057ec98e8dd889191931dadee9dabc64902b62
2YA <374779789@qq.com>
2023-05-17 17:22:31 +0800

    Update sub-store.sh

--- docker/sub-store.sh
+++ docker/sub-store.sh
@@ -30,7 +30,7 @@ echo -e "==============================================================\n"
 echo -e "======================== 3、启动后端接口 ========================\n"
 
     cp -r "$gitPath/Sub-Store/backend" "$rootPath"
-    cd "$backend" && pnpm build
+    cd "$backend" && pnpm install && pnpm build
     pm2 start sub-store.min.js --name "sub-store" --source-map-support --time
 
 echo -e "==============================================================\n"
@@ -46,9 +46,8 @@ echo -e "======================== 4、启动 Sub-Store 界面 ==================
         echo "VITE_API_URL = '${DOMAIN}'" >>"$web/.env.production"
     fi    
 
-    cd "$web"
     echo -e "执行编译前端静态资源\n"    
-    pnpm run build
+    cd "$web" && pnpm install && pnpm build
     echo -e "结束编译，UI 界面已生成\n"
 
 echo -e "==============================================================\n"
